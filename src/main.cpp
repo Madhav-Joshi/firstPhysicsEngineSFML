34803d4d908a9f56d585c3bed5f6969135ec896f
MaddyIITB <madhavjoshi@iitb.ac.in>
2025-03-04 22:18:10 +0530

    feat: final colorful variable size ball spawning simulation

--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,19 @@
 #include "events/events.hpp"
 #include "solver/solver.hpp"
 #include "renderer/renderer.hpp"
+#include "utils/math.hpp"
+#include "utils/number_generator.hpp"
 #include <iostream>
+
+static sf::Color getRainbow(float t)
+{
+    const float r = sin(t);
+    const float g = sin(t + 0.33f * 2.0f * Math::PI);
+    const float b = sin(t + 0.66f * 2.0f * Math::PI);
+    return {static_cast<uint8_t>(255.0f * r * r),
+            static_cast<uint8_t>(255.0f * g * g),
+            static_cast<uint8_t>(255.0f * b * b)};
+}
 
 int main()
 {
@@ -15,10 +27,13 @@ int main()
     Renderer renderer{window};
 
     // Set simulation attributes
-    const float object_spawn_delay = 0.05f;
-    const uint32_t max_objects_count = 200;
-    const sf::Vector2f object_spawn_position = sf::Vector2f(200.0f, 0.0f) + conf::window_size_f * 0.5f;
-    const float object_spawn_speed = 300.0f;
+    const float object_spawn_delay = 0.001f;
+    const uint32_t max_objects_count = 400;
+    const sf::Vector2f object_spawn_position = sf::Vector2f(0.0f, -400.0f) + conf::window_size_f * 0.5f;
+    const float object_spawn_speed = 900.0f;
+    const float object_min_radius = 10.0f;
+    const float object_max_radius = 30.0f;
+    const float max_angle = 1.5f;
 
     // Solver configurations
     Solver balls;
@@ -34,9 +49,12 @@ int main()
         if (balls.getObjectsCount() < max_objects_count && clock.getElapsedTime().asSeconds() >= object_spawn_delay)
         {
             clock.restart();
-            auto &ball = balls.addObject(object_spawn_position, conf::radius);
-            balls.setObjectVelocity(ball, object_spawn_speed * sf::Vector2f{0, 1});
-            ball.color = sf::Color::White;
+            auto &ball = balls.addObject(object_spawn_position, RNGf::getRange(object_min_radius, object_max_radius));
+
+            const float t = balls.getTime();
+            const float angle = max_angle * sin(t) + Math::PI * 0.5f;
+            balls.setObjectVelocity(ball, object_spawn_speed * sf::Vector2f{static_cast<float>(cos(angle)), static_cast<float>(sin(angle))});
+            ball.color = getRainbow(t);
         }
 
         balls.update();
